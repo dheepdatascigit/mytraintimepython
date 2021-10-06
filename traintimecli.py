39713c3a553df0c002f91491b57dabbf0ec9ff7a
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-10-07 00:55:48 +1100

    print cli train times

--- traintimecli.py
+++ traintimecli.py
@@ -15,6 +15,17 @@ def jsontofile(injson):
     with open('traintime.json', 'w') as outfile:
         json.dump(injson, outfile)
 
+def showtraintimes(injson):
+    """ show only train times in local time and departure platform
+    """
+
+    for i, ttid in enumerate(injson["departures"]):
+        print(f"next train {i}:")
+        print(f"platform_number: {ttid['platform_number']}")
+        print(f"scheduled_departure_utc: {ttid['scheduled_departure_utc']}")
+
+
+    
 
 url = f'http://timetableapi.ptv.vic.gov.au/v3/departures/route_type/0/stop/1044/route/3?direction_id=1&max_results=3&include_cancelled=false&include_geopath=true&devid={devid}&signature={signature}'
 response = requests.get(url)
@@ -24,6 +35,7 @@ print(json.dumps(out, indent=2))
 
 # write output json to local file
 jsontofile(out)
+showtraintimes(out)
 
 #print(len(out["results"]))
 
