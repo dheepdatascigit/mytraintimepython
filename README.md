17ce6f2e12c290d1eef9a91cb236a2784a070fbc
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-10-07 00:44:44 +1100

    json train times

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ can I buy coffee before my train in the morning at craigieburn station
 ## Setup
 - Virtual environment = venv_mytrainpython
 - pip run = pip install -r requirements.txt
+- Environment variables
+    - PTVAPI_DEVID = <developer id>
+    - PTVAPI_SIG = <signature of the api call>
--- traintimecli.py
+++ traintimecli.py
@@ -1,9 +1,32 @@
 import requests
+import json
+import os
 
-response = requests.get("https://randomuser.me/api/")
+devid = os.environ.get("PTVAPI_DEVID")
+signature = os.environ.get("PTVAPI_SIG")
+
+def jsontofile(injson):
+    """ write json output to file
+
+    input: json
+    output: traintime.json
+    """
+
+    with open('traintime.json', 'w') as outfile:
+        json.dump(injson, outfile)
+
+
+url = f'http://timetableapi.ptv.vic.gov.au/v3/departures/route_type/0/stop/1044/route/3?direction_id=1&max_results=3&include_cancelled=false&include_geopath=true&devid={devid}&signature={signature}'
+response = requests.get(url)
+
 out = response.json()
-print(len(out["results"]))
+print(json.dumps(out, indent=2))
 
-for i in out["results"][0]:
-    print(i, out["results"][0][i], '\n')
+# write output json to local file
+jsontofile(out)
+
+#print(len(out["results"]))
+
+# for i in out["results"][0]:
+#     print(i, out["results"][0][i], '\n')
 
